163f3d65cdbfa354234f1ef65058a2585de6e56c
Fredi Machado <fredisoft@gmail.com>
2025-04-28 17:09:57 +1000

    Automatically import Ollama credential when ready

--- N8nPlayground.AppHost/Extensions/N8nExtensions.cs
+++ N8nPlayground.AppHost/Extensions/N8nExtensions.cs
@@ -1,4 +1,7 @@
-﻿using System.Globalization;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+using System.Globalization;
 
 namespace N8nPlayground.AppHost.Extensions;
 
@@ -25,15 +28,81 @@ public static class N8nExtensions
                 RegionInfo.CurrentRegion.TwoLetterISORegionName,
                 out timeZone);
         }
-        
-        return builder
+
+        const string containerName = "n8n-playground";
+
+        var containerBuilder = builder
             .AddContainer(name, "n8nio/n8n", "1.89.2")
+            .WithContainerName(containerName)
             .WithEnvironment("GENERIC_TIMEZONE", timeZone)
             .WithEnvironment("TZ", timeZone)
             .WithEnvironment("N8N_RUNNERS_ENABLED", "true")
             .WithEnvironment("N8N_DIAGNOSTICS_ENABLED", "false")
             .WithEnvironment("N8N_PERSONALIZATION_ENABLED", "false")
+            .WithEnvironment("N8N_DEFAULT_BINARY_DATA_MODE", "filesystem")
+            .WithEnvironment("N8N_ENCRYPTION_KEY", "super-secret-key") // Changing this will invalidate all existing credentials
             .WithBindMount("../n8n/data", "/home/node/.n8n")
+            .WithBindMount("../n8n/credentials", "/shared-data/credentials")
             .WithHttpEndpoint(port, targetPort: 5678);
+
+        // Import existing credentials
+        builder.Eventing.Subscribe<ResourceReadyEvent>(containerBuilder.Resource, (@event, cancellationToken) =>
+        {
+            var loggerService = @event.Services.GetRequiredService<ResourceLoggerService>();
+
+            if (containerBuilder.Resource is not ContainerResource containerResource)
+            {
+                return Task.CompletedTask;
+            }
+
+            var logger = loggerService.GetLogger(containerBuilder.Resource);
+
+            _ = Task.Run(async () =>
+            {
+                var process = new Process
+                {
+                    StartInfo = new ProcessStartInfo
+                    {
+                        FileName = "cmd",
+                        Arguments = $"/c docker exec -d {containerName} sh -c \"sleep 3; n8n import:credentials --separate --input=/shared-data/credentials\"",
+                        RedirectStandardOutput = true,
+                        RedirectStandardError = true,
+                        CreateNoWindow = true,
+                        UseShellExecute = false,
+                    }
+                };
+
+                process.OutputDataReceived += (sender, args) =>
+                {
+                    if (!string.IsNullOrWhiteSpace(args.Data))
+                    {
+                        logger.LogInformation("{Data}", args.Data);
+                    }
+                };
+
+                process.ErrorDataReceived += (sender, args) =>
+                {
+                    if (!string.IsNullOrWhiteSpace(args.Data))
+                    {
+                        logger.LogError("{Data}", args.Data);
+                    }
+                };
+
+                process.Start();
+                process.BeginOutputReadLine();
+                process.BeginErrorReadLine();
+
+                await process.WaitForExitAsync(cancellationToken).ConfigureAwait(false);
+
+                if (process.ExitCode != 0)
+                {
+                    logger.LogError("Command exited with {ExitCode}", process.ExitCode);
+                }
+            }, cancellationToken);
+
+            return Task.CompletedTask;
+        });
+
+        return containerBuilder;
     }
 }
